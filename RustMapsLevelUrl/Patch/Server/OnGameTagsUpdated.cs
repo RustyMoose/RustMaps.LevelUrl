9a32a9aa183fcb05090bee8f789068cb4427706d
JJ <jexs@moose.gg>
2023-04-06 12:19:42 -0500

    add rustmaps gametag and set steamworks key for map URL

---
+++ RustMapsLevelUrl/Patch/Server/OnGameTagsUpdated.cs
@@ -0,0 +1,23 @@
+﻿using Harmony;
+using Steamworks;
+
+namespace RustMapsLevelUrl.Patch.Server
+{
+    [HarmonyPatch(typeof(SteamServer), nameof(SteamServer.GameTags), MethodType.Setter)]
+    public class OnGameTagsUpdated
+    {
+        public static void Prefix(ref string value)
+        {
+#if DEBUG
+            System.Console.WriteLine("RustMapsLevelUrl | OnGameTagsUpdated");
+#endif
+
+            if (value.EndsWith("rustmaps"))
+            {
+                return;
+            }
+            
+            value += ",rustmaps";
+        }
+    }
+}
---
+++ RustMapsLevelUrl/Patch/Server/OnServerInitialized.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using Steamworks;
+
+namespace RustMapsLevelUrl.Patch.Server
+{
+    [HarmonyPatch(typeof(ServerMgr), nameof(ServerMgr.OpenConnection))]
+    public static class OnServerInitialized
+    {
+        public static void Postfix()
+        {
+#if DEBUG
+            System.Console.WriteLine("RustMapsLevelUrl | OnServerInitialized");
+#endif
+
+            if (string.IsNullOrEmpty(ConVar.Server.levelurl))
+            {
+                return;
+            }
+
+            UnityEngine.Debug.Log($"RustMapsLevelUrl | Set Steamworks world.levelurl to '{ConVar.Server.levelurl}'");
+            SteamServer.SetKey("world.levelurl", ConVar.Server.levelurl);
+        }
+    }
+}
